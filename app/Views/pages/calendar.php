8550d3c5d4479008c03c01117ed8a020bdabc67a
J-Hyeonseo <47245112+J-HyeonSeo@users.noreply.github.com>
2025-04-04 12:03:19 +0900

    Feat: 일정 상세보기 모달 마크업.

--- app/Views/pages/calendar.php
+++ app/Views/pages/calendar.php
@@ -1,5 +1,44 @@
 <main>
     <div id="calendar"></div>
+    <aside id="detail-schedule">
+
+        <div id="close-schedule-btn">X</div>
+
+        <div id="type">회식</div>
+        <h4>1차 회식</h4>
+        <h6>장소: 닭갈비집</h6>
+        <h5>2024.4.1 16:00 ~</h5>
+        <h5>2024.4.1 18:30</h5>
+        <h4>참가자</h4>
+        <div id="participant-view">
+            <div class="participant-card">
+                Jerry
+            </div>
+            <div class="participant-card">
+                Jason
+            </div>
+            <div class="participant-card">
+                Tom
+            </div>
+            <div class="participant-card">
+                Patrick
+            </div>
+            <div class="participant-card">
+                Chris
+            </div>
+            <div class="participant-card">
+                James
+            </div>
+            <div class="participant-card">
+                Sarah
+            </div>
+        </div>
+        <div id="detail-schedule-btn-wrap">
+            <div class="btn" style="background-color: #F18800">수정</div>
+            <div class="btn" style="background-color: #B02A2A; margin: 0 10px;">삭제</div>
+            <div class="btn" style="background-color: #1F63BA;">복사</div>
+        </div>
+    </aside>
 </main>
 <script>
     document.addEventListener('DOMContentLoaded', function() {
@@ -37,7 +76,10 @@
                 },
             ],
             eventClick: function(info) {
-                alert(info.event.extendedProps.place)
+                alert(info.event.extendedProps.place);
+            },
+            dateClick: function(date) {
+                alert(date.dateStr);
             }
         });
         calendar.render();
