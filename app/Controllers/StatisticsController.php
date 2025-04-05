5768504180359aa8c00ff399b1c00d683b0041fb
J-Hyeonseo <47245112+J-HyeonSeo@users.noreply.github.com>
2025-04-05 17:58:10 +0900

    Feat: 백엔드 통계 API 5종 구현 완료.

--- app/Controllers/StatisticsController.php
+++ app/Controllers/StatisticsController.php
@@ -2,7 +2,269 @@
 
 namespace App\Controllers;
 
+use CodeIgniter\Database\Query;
+
 class StatisticsController extends BaseController
 {
+    private $scheduleModel;
+
+    public function __construct() {
+        $this->scheduleModel = new \App\Models\ScheduleModel();
+    }
+
+    // 일별 통계
+    public function getStatisticsDay() {
+
+        $startDate = $this->request->getGet('startDate');
+        $endDate = $this->request->getGet('endDate');
+
+        $pQuery = $this->scheduleModel->db->prepare(static function ($db) {
+
+            $sql = "
+            -- 일별 조회 쿼리
+            WITH RECURSIVE DATE_RANGE AS (
+                SELECT DATE(:startDate:) AS dt
+                UNION ALL
+                SELECT DATE_ADD(dt, INTERVAL 1 DAY)
+                FROM DATE_RANGE
+                WHERE dt < :endDate:
+            ),
+            SCHEDULE_DURATION AS (
+                SELECT
+                    D.dt AS schedule_date,
+                    CASE
+                        -- 데이터가 없으면, 0분
+                        WHEN S.schedule_id IS NULL THEN 0
+                        -- 둘 다 같은 날이면, end_dt - start_dt 값으로 계산
+                        WHEN DATE(S.start_dt) = DATE(S.end_dt) THEN TIMESTAMPDIFF(MINUTE, S.start_dt, S.end_dt)
+                        -- 시작 날짜가 오늘이면, 다음날 자정에서 - start_dt를 계산하여, start_dt부터 다음날 자정까지의 시간을 계산.
+                        WHEN D.dt = DATE(S.start_dt) THEN TIMESTAMPDIFF(MINUTE, S.start_dt, DATE_ADD(DATE(S.start_dt), INTERVAL 1 DAY))
+                        -- 종료 날짜가 오늘이면, end_dt에서 오늘 자정의 차이를 계산하여, 오늘 자정 ~ 마지막 시간 까지의 차이를 계산함.
+                        WHEN D.dt = DATE(S.end_dt) THEN TIMESTAMPDIFF(MINUTE, DATE(S.end_dt), S.end_dt)
+                        ELSE 1440
+                    END AS minutes
+                FROM DATE_RANGE D
+                LEFT JOIN SCHEDULE S
+                    ON D.dt BETWEEN DATE(S.start_dt) AND DATE(S.end_dt)
+            )
+            SELECT
+                schedule_date AS xData,
+                ROUND(SUM(minutes) / 60, 1) AS yData
+            FROM SCHEDULE_DURATION
+            GROUP BY schedule_date
+            ORDER BY schedule_date;
+            ";
+
+            return (new Query($db))->setQuery($sql);
+        });
+
+        $data = $pQuery->execute($startDate, $endDate)->getResult();
+
+        return $this->response->setJSON($data);
+    }
+
+    // 월별 통계
+    public function getStatisticsMonth() {
+        $startDate = $this->request->getGet('startDate');
+        $endDate = $this->request->getGet('endDate');
+
+        $pQuery = $this->scheduleModel->db->prepare(static function ($db) {
+
+            $sql = "
+            -- 월별 조회 쿼리
+            WITH RECURSIVE DATE_RANGE AS (
+                SELECT DATE(:startDate:) AS dt
+                UNION ALL
+                SELECT DATE_ADD(dt, INTERVAL 1 DAY)
+                FROM DATE_RANGE
+                WHERE dt < :endDate:
+            ),
+            SCHEDULE_DURATION AS (
+                SELECT
+                    DATE_FORMAT(D.dt, '%Y-%m') AS schedule_month,
+                    CASE
+                        -- 데이터가 없으면, 0분
+                        WHEN S.schedule_id IS NULL THEN 0
+                        -- 둘 다 같은 날이면, end_dt - start_dt 값으로 계산
+                        WHEN DATE(S.start_dt) = DATE(S.end_dt) THEN TIMESTAMPDIFF(MINUTE, S.start_dt, S.end_dt)
+                        -- 시작 날짜가 오늘이면, 다음날 자정에서 - start_dt를 계산하여, start_dt부터 다음날 자정까지의 시간을 계산.
+                        WHEN D.dt = DATE(S.start_dt) THEN TIMESTAMPDIFF(MINUTE, S.start_dt, DATE_ADD(DATE(S.start_dt), INTERVAL 1 DAY))
+                        -- 종료 날짜가 오늘이면, end_dt에서 오늘 자정의 차이를 계산하여, 오늘 자정 ~ 마지막 시간 까지의 차이를 계산함.
+                        WHEN D.dt = DATE(S.end_dt) THEN TIMESTAMPDIFF(MINUTE, DATE(S.end_dt), S.end_dt)
+                        ELSE 1440
+                    END AS minutes
+                FROM DATE_RANGE D
+                LEFT JOIN SCHEDULE S
+                    ON D.dt BETWEEN DATE(S.start_dt) AND DATE(S.end_dt)
+            )
+            SELECT
+                schedule_month AS xData,
+                ROUND(SUM(minutes) / 60, 1) AS yData
+            FROM SCHEDULE_DURATION
+            GROUP BY schedule_month
+            ORDER BY schedule_month;
+            ";
+
+            return (new Query($db))->setQuery($sql);
+        });
+
+        $data = $pQuery->execute($startDate, $endDate)->getResult();
+
+        return $this->response->setJSON($data);
+    }
+
+    //
+    public function getStatisticsParticipant() {
+        $startDate = $this->request->getGet('startDate');
+        $endDate = $this->request->getGet('endDate');
+
+        $pQuery = $this->scheduleModel->db->prepare(static function ($db) {
+
+            $sql = "
+            -- 참가자별 조회 쿼리
+            WITH RECURSIVE DATE_RANGE AS (
+                SELECT DATE(:startDate:) AS dt
+                UNION ALL
+                SELECT DATE_ADD(dt, INTERVAL 1 DAY)
+                FROM DATE_RANGE
+                WHERE dt < :endDate:
+            ),
+            SCHEDULE_DURATION AS (
+                SELECT
+                    DATE_FORMAT(D.dt, '%Y-%m') AS schedule_month,
+                    P.member_id AS member_id,
+                    CASE
+                        -- 데이터가 없으면, 0분
+                        WHEN S.schedule_id IS NULL THEN 0
+                        -- 둘 다 같은 날이면, end_dt - start_dt 값으로 계산
+                        WHEN DATE(S.start_dt) = DATE(S.end_dt) THEN TIMESTAMPDIFF(MINUTE, S.start_dt, S.end_dt)
+                        -- 시작 날짜가 오늘이면, 다음날 자정에서 - start_dt를 계산하여, start_dt부터 다음날 자정까지의 시간을 계산.
+                        WHEN D.dt = DATE(S.start_dt) THEN TIMESTAMPDIFF(MINUTE, S.start_dt, DATE_ADD(DATE(S.start_dt), INTERVAL 1 DAY))
+                        -- 종료 날짜가 오늘이면, end_dt에서 오늘 자정의 차이를 계산하여, 오늘 자정 ~ 마지막 시간 까지의 차이를 계산함.
+                        WHEN D.dt = DATE(S.end_dt) THEN TIMESTAMPDIFF(MINUTE, DATE(S.end_dt), S.end_dt)
+                        ELSE 1440
+                    END AS minutes
+                FROM DATE_RANGE D
+                JOIN SCHEDULE S
+                    ON D.dt BETWEEN DATE(S.start_dt) AND DATE(S.end_dt)
+                LEFT JOIN PARTICIPANT P
+                    ON P.schedule_id = S.schedule_id
+            )
+            SELECT
+                M.nickname AS xData,
+                ROUND(SUM(SD.minutes) / 60, 1) AS yData
+            FROM SCHEDULE_DURATION SD
+            JOIN MEMBER M ON M.member_id = SD.member_id
+            GROUP BY SD.member_id
+            ORDER BY yData DESC;
+            ";
+
+            return (new Query($db))->setQuery($sql);
+        });
+
+        $data = $pQuery->execute($startDate, $endDate)->getResult();
+
+        return $this->response->setJSON($data);
+    }
+
+    public function getStatisticsPlace() {
+        $startDate = $this->request->getGet('startDate');
+        $endDate = $this->request->getGet('endDate');
+
+        $pQuery = $this->scheduleModel->db->prepare(static function ($db) {
+
+            $sql = "
+            -- 장소별 통계 조회
+            WITH RECURSIVE DATE_RANGE AS (
+                SELECT DATE(:startDate:) AS dt
+                UNION ALL
+                SELECT DATE_ADD(dt, INTERVAL 1 DAY)
+                FROM DATE_RANGE
+                WHERE dt < :endDate:
+            ),
+            SCHEDULE_DURATION AS (
+                SELECT
+                    DATE_FORMAT(D.dt, '%Y-%m') AS schedule_month,
+                    place,
+                    CASE
+                        -- 데이터가 없으면, 0분
+                        WHEN S.schedule_id IS NULL THEN 0
+                        -- 둘 다 같은 날이면, end_dt - start_dt 값으로 계산
+                        WHEN DATE(S.start_dt) = DATE(S.end_dt) THEN TIMESTAMPDIFF(MINUTE, S.start_dt, S.end_dt)
+                        -- 시작 날짜가 오늘이면, 다음날 자정에서 - start_dt를 계산하여, start_dt부터 다음날 자정까지의 시간을 계산.
+                        WHEN D.dt = DATE(S.start_dt) THEN TIMESTAMPDIFF(MINUTE, S.start_dt, DATE_ADD(DATE(S.start_dt), INTERVAL 1 DAY))
+                        -- 종료 날짜가 오늘이면, end_dt에서 오늘 자정의 차이를 계산하여, 오늘 자정 ~ 마지막 시간 까지의 차이를 계산함.
+                        WHEN D.dt = DATE(S.end_dt) THEN TIMESTAMPDIFF(MINUTE, DATE(S.end_dt), S.end_dt)
+                        ELSE 1440
+                    END AS minutes
+                FROM DATE_RANGE D
+                JOIN SCHEDULE S
+                    ON D.dt BETWEEN DATE(S.start_dt) AND DATE(S.end_dt)
+            )
+            SELECT
+                place AS xData,
+                ROUND(SUM(minutes) / 60, 1) AS yData
+            FROM SCHEDULE_DURATION
+            GROUP BY place
+            ORDER BY yData DESC;
+            ";
+
+            return (new Query($db))->setQuery($sql);
+        });
+
+        $data = $pQuery->execute($startDate, $endDate)->getResult();
+
+        return $this->response->setJSON($data);
+    }
+
+    public function getStatisticsType() {
+        $startDate = $this->request->getGet('startDate');
+        $endDate = $this->request->getGet('endDate');
+
+        $pQuery = $this->scheduleModel->db->prepare(static function ($db) {
+
+            $sql = "
+            -- 종류별 통계 조회
+            WITH RECURSIVE DATE_RANGE AS (
+                SELECT DATE(:startDate:) AS dt
+                UNION ALL
+                SELECT DATE_ADD(dt, INTERVAL 1 DAY)
+                FROM DATE_RANGE
+                WHERE dt < :endDate:
+            ),
+            SCHEDULE_DURATION AS (
+                SELECT
+                    DATE_FORMAT(D.dt, '%Y-%m') AS schedule_month,
+                    type,
+                    CASE
+                        -- 데이터가 없으면, 0분
+                        WHEN S.schedule_id IS NULL THEN 0
+                        -- 둘 다 같은 날이면, end_dt - start_dt 값으로 계산
+                        WHEN DATE(S.start_dt) = DATE(S.end_dt) THEN TIMESTAMPDIFF(MINUTE, S.start_dt, S.end_dt)
+                        -- 시작 날짜가 오늘이면, 다음날 자정에서 - start_dt를 계산하여, start_dt부터 다음날 자정까지의 시간을 계산.
+                        WHEN D.dt = DATE(S.start_dt) THEN TIMESTAMPDIFF(MINUTE, S.start_dt, DATE_ADD(DATE(S.start_dt), INTERVAL 1 DAY))
+                        -- 종료 날짜가 오늘이면, end_dt에서 오늘 자정의 차이를 계산하여, 오늘 자정 ~ 마지막 시간 까지의 차이를 계산함.
+                        WHEN D.dt = DATE(S.end_dt) THEN TIMESTAMPDIFF(MINUTE, DATE(S.end_dt), S.end_dt)
+                        ELSE 1440
+                    END AS minutes
+                FROM DATE_RANGE D
+                JOIN SCHEDULE S
+                    ON D.dt BETWEEN DATE(S.start_dt) AND DATE(S.end_dt)
+            )
+            SELECT
+                REPLACE(REPLACE(REPLACE(REPLACE(type, 'GENERAL', '일반'), 'EDUCATION', '교육'), 'SEMINAR', '세미나'), 'STAFFPARTY', '회식') AS xData,
+                ROUND(SUM(minutes) / 60, 1) AS yData
+            FROM SCHEDULE_DURATION
+            GROUP BY type
+            ORDER BY yData DESC;
+            ";
+
+            return (new Query($db))->setQuery($sql);
+        });
+
+        $data = $pQuery->execute($startDate, $endDate)->getResult();
+
+        return $this->response->setJSON($data);
+    }
 
 }
