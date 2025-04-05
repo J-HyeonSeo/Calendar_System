d20b6894399b310449253d579273553c7d0273a5
J-Hyeonseo <47245112+J-HyeonSeo@users.noreply.github.com>
2025-04-05 18:42:00 +0900

    Feat: 통계 API 연계 작업.

--- app/Views/pages/statistics.php
+++ app/Views/pages/statistics.php
@@ -1,59 +1,59 @@
 
-<div class="statistics-area">
+<div class="statistics-area" id="day-statistics">
     <div class="search-form-wrap">
         <h2 class="statistics-title">일별 할당된 일정 통계</h2>
         <form class="search-form">
-            <input type="date"/> -
-            <input type="date"/>
-            <img class="search-btn" alt="search" src="/public/images/search.png"/>
+            <input class="start-date" type="date"/> -
+            <input class="end-date" type="date"/>
+            <img class="search-btn" onclick="dataCallAndSettingChart('day')" alt="search" src="/public/images/search.png"/>
         </form>
     </div>
     <div id="day-chart" class="chart-content"></div>
 </div>
 
-<div class="statistics-area">
+<div class="statistics-area" id="month-statistics">
     <div class="search-form-wrap">
         <h2 class="statistics-title">월별 할당된 일정 통계</h2>
         <form class="search-form">
-            <input type="date"/> -
-            <input type="date"/>
-            <img class="search-btn" alt="search" src="/public/images/search.png"/>
+            <input class="start-date" type="date"/> -
+            <input class="end-date" type="date"/>
+            <img class="search-btn" onclick="dataCallAndSettingChart('month')" alt="search" src="/public/images/search.png"/>
         </form>
     </div>
     <div id="month-chart" class="chart-content"></div>
 </div>
 
-<div class="statistics-area">
+<div class="statistics-area" id="participant-statistics">
     <div class="search-form-wrap">
         <h2 class="statistics-title">참가자별 할당된 일정 통계</h2>
         <form class="search-form">
-            <input type="date"/> -
-            <input type="date"/>
-            <img class="search-btn" alt="search" src="/public/images/search.png"/>
+            <input class="start-date" type="date"/> -
+            <input class="end-date" type="date"/>
+            <img class="search-btn" onclick="dataCallAndSettingChart('participant')" alt="search" src="/public/images/search.png"/>
         </form>
     </div>
     <div id="participant-chart" class="chart-content"></div>
 </div>
 
-<div class="statistics-area">
+<div class="statistics-area" id="place-statistics">
     <div class="search-form-wrap">
         <h2 class="statistics-title">장소별 할당된 일정 통계</h2>
         <form class="search-form">
-            <input type="date"/> -
-            <input type="date"/>
-            <img class="search-btn" alt="search" src="/public/images/search.png"/>
+            <input class="start-date" type="date"/> -
+            <input class="end-date" type="date"/>
+            <img class="search-btn" onclick="dataCallAndSettingChart('place')" alt="search" src="/public/images/search.png"/>
         </form>
     </div>
     <div id="place-chart" class="chart-content"></div>
 </div>
 
-<div class="statistics-area">
+<div class="statistics-area" id="type-statistics">
     <div class="search-form-wrap">
         <h2 class="statistics-title">종류별 할당된 일정 통계</h2>
         <form class="search-form">
-            <input type="date"/> -
-            <input type="date"/>
-            <img class="search-btn" alt="search" src="/public/images/search.png"/>
+            <input class="start-date" type="date"/> -
+            <input class="end-date" type="date"/>
+            <img class="search-btn" onclick="dataCallAndSettingChart('type')" alt="search" src="/public/images/search.png"/>
         </form>
     </div>
     <div id="type-chart" class="chart-content"></div>
@@ -70,49 +70,120 @@
     const placeChart = echarts.init(document.getElementById('place-chart'));
     const typeChart = echarts.init(document.getElementById('type-chart'));
 
-    const dayData = [
-        { xData: '2024-04-01', yData: 8.5 },
-        { xData: '2024-04-02', yData: 9.2 },
-        { xData: '2024-04-03', yData: 27.8 },
-        { xData: '2024-04-04', yData: 100.0 },
-        { xData: '2024-04-05', yData: 6.5 }
-    ];
-    settingChart(dayChart, '일별 통계', dayData, '#600c6a');
+    // 오늘 날을 기준으로, 월초, 월말 구하기.
+    const today = new Date();
+    const startOfMonth = new Date(today.getFullYear(), today.getMonth(), 1);
+    const endOfMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0);
 
-    const monthData = [
-        { xData: '2024-04', yData: 8.5 },
-        { xData: '2024-05', yData: 9.2 },
-        { xData: '2024-06', yData: 27.8 },
-        { xData: '2024-07', yData: 140.0 },
-        { xData: '2024-08', yData: 6.5 }
-    ];
-    settingChart(monthChart, '월별 통계', monthData, '#18937e')
+    const formatDate = (date) => {
+        const yyyy = date.getFullYear();
+        const mm = String(date.getMonth() + 1).padStart(2, '0');
+        const dd = String(date.getDate()).padStart(2, '0');
+        return `${yyyy}-${mm}-${dd}`;
+    };
 
-    const participantData = [
-        { xData: 'Jerry', yData: 8.5 },
-        { xData: 'Jason', yData: 26.2 },
-        { xData: 'Patrick', yData: 27.8 },
-        { xData: 'James', yData: 100.0 },
-        { xData: 'Sarah', yData: 6.5 }
-    ];
-    settingChart(participantChart, '참가자별 통계', participantData, '#11983e');
+    $('.start-date').val(formatDate(startOfMonth));
+    $('.end-date').val(formatDate(endOfMonth));
 
-    const placeData = [
-        { xData: '롯데월드', yData: 66.5 },
-        { xData: '닭갈비집', yData: 26.2 },
-        { xData: '레고랜드', yData: 27.8 },
-        { xData: '에버랜드', yData: 100.0 },
-        { xData: '롯데시그니엘호텔', yData: 6.5 }
-    ];
-    settingChart(placeChart, '장소별 통계', placeData, '#d67615');
 
-    const typeData = [
-        { xData: '일반', yData: 67.5 },
-        { xData: '교육', yData: 26.2 },
-        { xData: '세미나', yData: 27.8 },
-        { xData: '회식', yData: 100.0 },
-    ];
-    settingChart(typeChart, '종류별 통계', typeData);
+    // 최초 그래프 할당하기.
+    dataCallAndSettingChart('day');
+    dataCallAndSettingChart('month');
+    dataCallAndSettingChart('participant');
+    dataCallAndSettingChart('place');
+    dataCallAndSettingChart('type');
+
+    // 차트 유형과 날짜 범위를 입력받아서, 셋팅.
+    function dataCallAndSettingChart(chartType) {
+
+        const startDate = $(`#${chartType}-statistics .start-date`).val();
+        const endDate = $(`#${chartType}-statistics .end-date`).val();
+
+        $.ajax({
+            url: `/statistics/${chartType}?startDate=${startDate}&endDate=${endDate}`,
+            type: 'GET', // HTTP 메서드
+            success: function (response) {
+                const chartList = response.chartList;
+                switch (chartType) {
+                    case 'day':
+                        settingChart(dayChart, '일별 통계', chartList, '#600c6a');
+                        break;
+                    case 'month':
+                        settingChart(monthChart, '월별 통계', chartList, '#18937e');
+                        break;
+                    case 'participant':
+                        settingChart(participantChart, '참가자별 통계', chartList, '#11983e');
+                        break;
+                    case 'place':
+                        settingChart(placeChart, '장소별 통계', chartList, '#d67615');
+                        break;
+                    case 'type':
+                        settingChart(typeChart, '종류별 통계', chartList);
+                        break;
+                }
+            },
+            error: function () {
+                console.error(chartType + "에 대한 차트정보를 조회하지 못하였음.");
+            }
+        });
+    }
+
+
+
+
+
+
+
+
+
+    // const dayData = [
+    //     { xData: '2024-04-01', yData: 8.5 },
+    //     { xData: '2024-04-02', yData: 9.2 },
+    //     { xData: '2024-04-03', yData: 27.8 },
+    //     { xData: '2024-04-04', yData: 100.0 },
+    //     { xData: '2024-04-05', yData: 6.5 }
+    // ];
+    // settingChart(dayChart, '일별 통계', dayData, '#600c6a');
+    //
+    // const monthData = [
+    //     { xData: '2024-04', yData: 8.5 },
+    //     { xData: '2024-05', yData: 9.2 },
+    //     { xData: '2024-06', yData: 27.8 },
+    //     { xData: '2024-07', yData: 140.0 },
+    //     { xData: '2024-08', yData: 6.5 }
+    // ];
+    // settingChart(monthChart, '월별 통계', monthData, '#18937e')
+    //
+    // const participantData = [
+    //     { xData: 'Jerry', yData: 8.5 },
+    //     { xData: 'Jason', yData: 26.2 },
+    //     { xData: 'Patrick', yData: 27.8 },
+    //     { xData: 'James', yData: 100.0 },
+    //     { xData: 'Sarah', yData: 6.5 }
+    // ];
+    // settingChart(participantChart, '참가자별 통계', participantData, '#11983e');
+    //
+    // const placeData = [
+    //     { xData: '롯데월드', yData: 66.5 },
+    //     { xData: '닭갈비집', yData: 26.2 },
+    //     { xData: '레고랜드', yData: 27.8 },
+    //     { xData: '에버랜드', yData: 100.0 },
+    //     { xData: '롯데시그니엘호텔', yData: 6.5 }
+    // ];
+    // settingChart(placeChart, '장소별 통계', placeData, '#d67615');
+    //
+    // const typeData = [
+    //     { xData: '일반', yData: 67.5 },
+    //     { xData: '교육', yData: 26.2 },
+    //     { xData: '세미나', yData: 27.8 },
+    //     { xData: '회식', yData: 100.0 },
+    // ];
+    // settingChart(typeChart, '종류별 통계', typeData);
+
+
+
+
+
 
     // 차트를 새롭게 셋팅하는 함수
     function settingChart(chart, title, chartData, color='#000000') {
@@ -120,7 +191,7 @@
         // x축 데이터
         const xAxisData = chartData.map(item => item.xData);
         // y축 데이터
-        const yAxisData = chartData.map(item => item.yData);
+        const yAxisData = chartData.map(item => Number(item.yData));
 
         // y축 최소/최대 자동 계산
         const minTime = Math.floor(Math.min(...yAxisData)) - 1; // 최소값보다 1시간 낮게
